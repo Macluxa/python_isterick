7d565f79ab3ebb58014598fdcfc147f0ad086300
Macluxa <macluxa.m@gmail.com>
2016-03-29 21:13:49 +0300

    create new values arguments group

--- create_new_group.py
+++ create_new_group.py
@@ -2,6 +2,8 @@
 
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
+
 
 def is_alert_present(wd):
     try:
@@ -18,37 +20,37 @@ class test_new_group(unittest.TestCase):
     def test_test_new_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, 'admin', 'secret')
         self.opens_group(wd)
-        self.create_name_new_group(wd)
+        self.create_name_new_group(wd, Group(name='test test', header='test test', footer='test test'))
         self.return_page_list_group(wd)
 
     def return_page_list_group(self, wd):
         wd.find_elements_by_link_text('group page').click()
 
-    def create_name_new_group(self, wd):
+    def create_name_new_group(self, wd,   group):
+        wd.find_element_by_class_name('new').click()
         wd.find_element_by_name('group_name').click()
         wd.find_element_by_name('group_name').clear()
-        wd.find_element_by_name('group_name').send_keys('test test')
+        wd.find_element_by_name('group_name').send_keys(group.name)
         wd.find_element_by_name('group_header').click()
         wd.find_element_by_name('group_header').clear()
-        wd.find_element_by_name('group_header').send_keys('test test')
+        wd.find_element_by_name('group_header').send_keys(group.header)
         wd.find_element_by_name('group_footer').click()
         wd.find_element_by_name('group_footer').clear()
-        wd.find_element_by_name('group_footer').send_keys('test test')
+        wd.find_element_by_name('group_footer').send_keys(group.footer)
 
 
     def opens_group(self, wd):
         wd.find_elements_by_link_text('groups').click()
 
-    def login(self, wd):
-        wd.find_element_by_class_name('new').click()
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys('admin')
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys('secret')
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
         wd.find_element_by_name("submit").click()
 
